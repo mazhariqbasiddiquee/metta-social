38e02f066b56d2a5a4396b527d320944ad37c788
mazhariqbasiddiquee <mazhariqbal963169u@gamil.com>
2024-01-31 15:57:18 +0530

    KL

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,96 @@
-
 import './App.css';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
- let [data,change_data]=useState([])
+  let [data,change_data]=useState([])
   let [currency1,change_currency]=useState("")
-  let [total,change_count_button]=useState([])
-  let [curr,curr_change]=useState(1)
-  useEffect(()=>{
-    fetch("https://restcountries.com/v3.1/all")
-    .then((res)=>res.json())
-    .then((data)=>{
-      let length=Math.ceil(data.length/20)
-      
-     
-      
-      
-      let arr=[]
-      for(let i=1;i<=length;i++){
-           arr.push(i)
+ let [total,change_count_button]=useState([])
+  let [currentPage, set_Current_Page] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let url =
+      currency1 === ''
+          ? 'https://restcountries.com/v3.1/all'
+          : `https://restcountries.com/v3.1/currency/${currency1}`;
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.status === 404) {
+          change_data([]);
+          change_count_button([]);
+        } else {
+          let length = Math.ceil(data.length / 20);
+          let arr = [];
+          for (let i = 1; i <= length; i++) {
+            arr.push(i);
+          }
+          change_count_button(arr);
+          let init=(currentPage-1)*20;
+          let final= currentPage*20;
+          let slicedData=data.slice(init, final);
+          change_data(slicedData);
+        }
+      } catch (error) {
+        console.log(error);
       }
-        
-      change_count_button(arr)
-      console.log(curr,"**",data.length)
-      let init = (curr-1)*20;
-      let final = curr*20;
-      data = data.slice(init, final);
-      change_data(data)
+    };
 
-      
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
+    const debounceTimer = setTimeout(() => {
+      fetchData();
+    }, 500); 
 
-  },[curr])
+    return () => clearTimeout(debounceTimer);
+  }, [currency1, currentPage]);
 
- const  Handlesubmit=()=>{
-  
-  let url=currency1===""?"https://restcountries.com/v3.1/all":`https://restcountries.com/v3.1/currency/${currency1}`
-  fetch(url)
-  .then((res)=>res.json())
-  .then((data)=>{
-    if(data.status===404){
-      change_data([])
-
-      curr_change([])
-    }
-    else{
-      let length=Math.ceil(data.length/20)
-     
-      let arr=[]
-      for(let i=1;i<=length;i++){
-           arr.push(i)
-      }
-      
-      change_count_button(arr)
-      if(data.length<=20){
-        change_data(data)
-
-      }
-      else{
-        let init = (curr-1)*20;
-      let final = curr*20;
-      data = data.slice(init, final);
-      change_data(data)
-
-      }
-      
-    }
-  
-  })
-  .catch((err)=>{
-    console.log(err)
-    
-  })
- }
+  const handleInputChange = (e) => {
+    change_currency(e.target.value);
+    set_Current_Page(1); 
+  };
 
   return (
     <div>
-    <h1 style={{textAlign:"center"}}>World by Currency</h1>
-      <div style={{textAlign:"center"}}>
-          <input type="text"  placeholder='Enter currency name to search'        style={{width:"20%", height:"24px",marginTop:"5%"}} onChange={(e)=>{
-               change_currency(e.target.value)
-          }} />
-           <button onClick={Handlesubmit}  style={{width:"8%",marginLeft:"20px",height:"24px"}}>Search</button>
+      <h1 style={{ textAlign: 'center' }}>World by Currency</h1>
+      <div style={{ textAlign: 'center',minWidth:"150px" }}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter currency name to search"
+          style={{}}
+          onChange={handleInputChange}
+        />
       </div>
-      <div className='content'>{
-        data.length===0?<h1 >No country matches</h1>:
-        data.map(({name,flags,capital})=>{
-   
-          return(
-            <div>
-                <img src={flags.png} alt="error" />
-                <div style={{textAlign:"center", margin:"40px"}}>
-                <h3>{name.official}</h3>
-                <p>{capital}</p>
+      <div className="content">
+        {data.length === 0 ? (
+          <h1>No country matches</h1>
+        ) : (
+          data.map(({ name, flags, capital }) => {
+            return (
+              <div   style={{display:"grid",placeItems:"center"}}  key={name.official}>
+                <img src={flags.png} alt="error"  className='image' />
+                <div style={{ textAlign: 'center', margin: '40px' }}>
+                  <h3>{name.official}</h3>
+                  <p>{capital}</p>
                 </div>
-              
-
-            </div>
-          )
-        })
-        
-        
-        }</div>
-            <div style={{display:"flex",justifyContent:"center",marginBottom:"50px"}}>{
-              total.map((elem)=>{
-                return(<button style={{marginLeft:"10px",width:"30px"}} onClick={()=>{
-                  curr_change(elem)
-                }}>
-                  {elem}
-                </button>)
-              })
-              
-              }</div>
+              </div>
+            );
+          })
+        )}
+      </div>
+      <div className="button">
+        {total.map((elem) => (
+          <button
+            key={elem}
+            style={{ marginLeft: '10px', width: '30px' }}
+            onClick={() => {
+              set_Current_Page(elem);
+            }}
+          >
+            {elem}
+          </button>
+        ))}
+      </div>
     </div>
-    
   );
 }
 
